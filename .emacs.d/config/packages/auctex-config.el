5c90bb3dbf3266fb720e3d0b62ee51a676caf16a
yusekiya <y.seki.14870@gmail.com>
2015-10-23 04:34:18 +0900

    Add config file for auctex

---
+++ .emacs.d/config/packages/auctex-config.el
@@ -0,0 +1,8 @@
+(use-package auctex
+  :mode (("\\.tex$" . yatex-mode)
+         ("\\.ltx$" . yatex-mode)
+         ("\\.cls$" . yatex-mode)
+         ("\\.sty$" . yatex-mode)
+         ("\\.clo$" . yatex-mode)
+         ("\\.bbl$" . yatex-mode))
+  )
