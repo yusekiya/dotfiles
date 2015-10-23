70f5bd78411a26ebdf675afdbb9ec86716ad441d
yusekiya <y.seki.14870@gmail.com>
2015-10-24 06:15:37 +0900

    Improve setup.sh
    - Add explanation for setup
    - Add confirmation before starting setup
    - Deal with the case that a symbolic link has the same name
      as a file in target directory

--- setup.sh
+++ setup.sh
@@ -1,21 +1,54 @@
 #!/usr/bin/env bash
-cdir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+dist_dir=~
 
-for f in ${cdir}/.??*
+echo "This script makes symbolic links to files in ${source_dir}."
+echo "The links will be created in ${dist_dir}."
+echo "When a link duplicates with a file in ${dist_dir},"
+echo "the file will be renamed by adding a suffix \".bak\"."
+echo 
+echo -n "Are you sure to continue? [yes/no] "
+read answer
+if [ "$answer" != "yes" ]; then
+    echo "Setup aborted"
+    exit;
+fi
+
+echo "==== log output ===="
+for f in ${source_dir}/.??*
 do
     [[ "$( basename "$f")" == ".git" ]] && continue
     [[ "$( basename "$f")" == ".gitignore" ]] && continue
     [[ "$( basename "$f")" == ".DS_Store" ]] && continue
 
+    dist="${dist_dir}/$( basename "$f")"
+
     if [ -d "$f" ]; then
-        ln -sfn "$f" ~/"$( basename "$f")"
-        echo Directory symbolic link created: ~/"$( basename "$f")"/
+        if [ -L ${dist} ]; then
+            ln -snf -S ".bak" "$f" ${dist}
+            echo Existing link ${dist} renamed to ${dist}.bak
+        elif [ -d ${dist} ]; then
+            mv ${dist} ${dist}.bak
+            ln -s "$f" ${dist}
+            echo Existing regular directory ${dist} renamed to ${dist}.bak
+        else
+            ln -s "$f" ${dist}
+        fi
+        echo Directory symbolic link created: ${dist}/
     fi
     
     if [ -f "$f" ]; then
-        ln -sf "$f" ~/"$( basename "$f")"
-        echo File symbolic link created: ~/"$( basename "$f")"
+        if [ -L ${dist} ]; then
+            ln -snf -S ".bak" "$f" ${dist}
+            echo Existing link ${dist} renamed to ${dist}.bak
+        elif [ -f ${dist} ]; then
+            mv ${dist} ${dist}.bak
+            ln -s "$f" ${dist}
+            echo Existing regular file ${dist} renamed to ${dist}.bak
+        else
+            ln -s "$f" ${dist}
+        fi
+        echo File symbolic link created: ${dist}
     fi
     
 done
-
