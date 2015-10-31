fac78549afd918dcf0d2773ada8f6c5b6d82efd1
yusekiya <y.seki.14870@gmail.com>
2015-10-31 19:43:31 +0900

    Modify emacs install procedure

--- .pkgbuild/mingw-w64-emacs/makepkg.sh
+++ .pkgbuild/mingw-w64-emacs/makepkg.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
-MINGW_CHOST=x86_64-w64-mingw32 MINGW_PACKAGE_PREFIX=mingw-w64-x86_64 MINGW_PREFIX=/mingw64 makepkg -s --skippgpcheck
+CARCH="x86_64" MINGW_CHOST=x86_64-w64-mingw32 MINGW_PACKAGE_PREFIX=mingw-w64-x86_64 MINGW_PREFIX=/mingw64 makepkg -s --skippgpcheck
 pacman -U *.pkg.tar.xz
+find . -type f -name "config.log" | xargs -i cp {} .
 rm -rf pkg src emacs* *.pkg.tar.xz
