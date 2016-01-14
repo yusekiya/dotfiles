cff1297eab8dfbbe4b7495ffe9f9e3ac48625edd
yusekiya <y.seki.14870@gmail.com>
2016-01-14 21:43:20 +0900

    Disable shell setting in emacs

--- .emacs.d/config/package_config.el
+++ .emacs.d/config/package_config.el
@@ -1493,18 +1493,18 @@ The argument icon must be string."
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; shell
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Check existence shell
-(defun skt:shell ()
-  (or (executable-find "zsh")
-      (executable-find "bash")
-      ;; (executable-find "f_zsh") ;; In case of Emacs + Cygwin
-      ;; (executable-find "f_bash") ;; In case of Emacs + Cygwin
-      (executable-find "cmdproxy")
-      (error "can't find 'shell' command in PATH!!")))
+;; ;; Check existence shell
+;; (defun skt:shell ()
+;;   (or (executable-find "zsh")
+;;       (executable-find "bash")
+;;       ;; (executable-find "f_zsh") ;; In case of Emacs + Cygwin
+;;       ;; (executable-find "f_bash") ;; In case of Emacs + Cygwin
+;;       (executable-find "cmdproxy")
+;;       (error "can't find 'shell' command in PATH!!")))
 
-(setq shell-file-name (skt:shell))
-(setenv "SHELL" shell-file-name)
-(setq explicit-shell-file-name shell-file-name)
+;; (setq shell-file-name (skt:shell))
+;; (setenv "SHELL" shell-file-name)
+;; (setq explicit-shell-file-name shell-file-name)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
