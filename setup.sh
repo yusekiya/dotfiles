5dd9981524bdfbf7e9d6dd5bbc51e209491bc449
yusekiya <y.seki.14870@gmail.com>
2016-01-16 20:01:07 +0900

    Improve setup script

--- setup.sh
+++ setup.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
 source_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 dest_dir=~
+ignore_list=(".git" ".gitignore" ".DS_Store")
 
-echo "This script makes symbolic links to files in ${source_dir}."
-echo "The links will be created in ${dest_dir}."
-echo "When a link duplicates with a file in ${dest_dir},"
-echo "the file will be renamed by adding a suffix \".bak\"."
-echo 
-echo -n "Are you sure to continue? [yes/no] "
-read answer
-if [ "$answer" != "yes" ]; then
-    echo "Setup aborted"
-    exit;
-fi
+usage_exit() {
+        echo "Usage: $( basename $0) [-h] [-f]" 1>&2
+        echo
+        echo "Options:"
+        echo "  -h  show this help"
+        echo "  -f  do not prompt before creating links in ${dest_dir}"
+        exit 1
+}
 
 # Windows needs administrator privilege to make symbolic link
 if [ "$(uname -o)" = "Msys" -a -z "$(id -Gn | grep Administrators)" ]; then
@@ -21,58 +19,167 @@ if [ "$(uname -o)" = "Msys" -a -z "$(id -Gn | grep Administrators)" ]; then
     exit;
 fi
 
-echo "==== log output ===="
-for f in ${source_dir}/.??*
-do
-    [[ "$( basename "$f")" == ".git" ]] && continue
-    [[ "$( basename "$f")" == ".gitignore" ]] && continue
-    [[ "$( basename "$f")" == ".DS_Store" ]] && continue
 
-    dest="${dest_dir}/$( basename "$f")"
-
-    if [ -d "$f" ]; then
-        if [ -L ${dest} ]; then
-            ln -snf -S ".bak" "$f" ${dest}
-            echo Existing link ${dest} renamed to ${dest}.bak
-        elif [ -d ${dest} ]; then
-            mv ${dest} ${dest}.bak
-            ln -s "$f" ${dest}
-            echo Existing regular directory ${dest} renamed to ${dest}.bak
-        else
-            ln -s "$f" ${dest}
-        fi
-        echo Directory symbolic link created: ${dest}/
+make_link_safely() {
+    echo "This script makes symbolic links to files in ${source_dir}."
+    echo "The links will be created in ${dest_dir}."
+    echo "When a link duplicates with a file in ${dest_dir},"
+    echo "the file will be renamed by adding a suffix \".bak\"."
+    echo 
+    echo -n "Are you sure to continue? [yes/no] "
+    read answer
+    if [ "$answer" != "yes" ]; then
+        echo "Setup aborted"
+        exit;
     fi
-    
-    if [ -f "$f" ]; then
+
+    echo "==== log output ===="
+    for f in ${source_dir}/.??*
+    do
+        flag_ignore=false
+        for ignore_file in "${ignore_list[@]}"
+        do
+            if [ "$( basename "$f")" = $ignore_file ]; then
+                flag_ignore=true
+                continue
+            fi
+        done
+        if $flag_ignore; then
+            continue
+        fi
+        
+        dest="${dest_dir}/$( basename "$f")"
+
+        if [ -d "$f" ]; then
+            if [ -L ${dest} ]; then
+                ln -snf -S ".bak" "$f" ${dest}
+                echo Existing link ${dest} renamed to ${dest}.bak
+            elif [ -d ${dest} ]; then
+                mv ${dest} ${dest}.bak
+                ln -s "$f" ${dest}
+                echo Existing regular directory ${dest} renamed to ${dest}.bak
+            else
+                ln -s "$f" ${dest}
+            fi
+            echo Directory symbolic link created: ${dest}/
+        fi
+
+        if [ -f "$f" ]; then
+            if [ -L ${dest} ]; then
+                ln -snf -S ".bak" "$f" ${dest}
+                echo Existing link ${dest} renamed to ${dest}.bak
+            elif [ -f ${dest} ]; then
+                mv ${dest} ${dest}.bak
+                ln -s "$f" ${dest}
+                echo Existing regular file ${dest} renamed to ${dest}.bak
+            else
+                ln -s "$f" ${dest}
+            fi
+            echo File symbolic link created: ${dest}
+        fi
+
+    done
+
+    # Create symbolic link ~/.profile which points to .bash_profile on linux
+    if [ "$(uname)" = "Linux" -a -f "${source_dir}/.bash_profile" ]; then
+        src="${source_dir}/.bash_profile"
+        dest="${dest_dir}/.profile"
         if [ -L ${dest} ]; then
-            ln -snf -S ".bak" "$f" ${dest}
+            ln -snf -S ".bak" ${src} ${dest}
             echo Existing link ${dest} renamed to ${dest}.bak
         elif [ -f ${dest} ]; then
             mv ${dest} ${dest}.bak
-            ln -s "$f" ${dest}
+            ln -s ${src} ${dest}
             echo Existing regular file ${dest} renamed to ${dest}.bak
         else
-            ln -s "$f" ${dest}
+            ln -s ${src} ${dest}
         fi
         echo File symbolic link created: ${dest}
     fi
-    
+}
+
+
+make_link_forcibly() {
+    echo "==== log output ===="
+    for f in ${source_dir}/.??*
+    do
+        flag_ignore=false
+        for ignore_file in "${ignore_list[@]}"
+        do
+            if [ "$( basename "$f")" = $ignore_file ]; then
+                flag_ignore=true
+                continue
+            fi
+        done
+        if $flag_ignore; then
+            continue
+        fi
+
+        dest="${dest_dir}/$( basename "$f")"
+
+        if [ -d "$f" ]; then
+            if [ -L ${dest} ]; then
+                ln -snf "$f" ${dest}
+                echo Existing link ${dest} renamed to ${dest}.bak
+            elif [ -d ${dest} ]; then
+                mv ${dest} ${dest}.bak
+                ln -s "$f" ${dest}
+                echo Existing regular directory ${dest} renamed to ${dest}.bak
+            else
+                ln -s "$f" ${dest}
+            fi
+            echo Directory symbolic link created: ${dest}/
+        fi
+
+        if [ -f "$f" ]; then
+            if [ -L ${dest} ]; then
+                ln -snf "$f" ${dest}
+                echo Existing link ${dest} renamed to ${dest}.bak
+            elif [ -f ${dest} ]; then
+                mv ${dest} ${dest}.bak
+                ln -s "$f" ${dest}
+                echo Existing regular file ${dest} renamed to ${dest}.bak
+            else
+                ln -s "$f" ${dest}
+            fi
+            echo File symbolic link created: ${dest}
+        fi
+
+    done
+
+    # Create symbolic link ~/.profile which points to .bash_profile on linux
+    if [ "$(uname)" = "Linux" -a -f "${source_dir}/.bash_profile" ]; then
+        src="${source_dir}/.bash_profile"
+        dest="${dest_dir}/.profile"
+        if [ -L ${dest} ]; then
+            ln -snf ${src} ${dest}
+        elif [ -f ${dest} ]; then
+            mv ${dest} ${dest}.bak
+            ln -s ${src} ${dest}
+            echo Existing regular file ${dest} renamed to ${dest}.bak
+        else
+            ln -s ${src} ${dest}
+        fi
+        echo File symbolic link created: ${dest}
+    fi
+}
+
+
+flag_force=false
+while getopts :hf OPT
+do
+    case $OPT in
+        h)  usage_exit
+            ;;
+        f)  flag_force=true
+            ;;
+        \?) usage_exit
+            ;;
+    esac
 done
 
-# Create symbolic link ~/.profile which points to .bash_profile on linux
-if [ "$(uname)" = "Linux" -a -f "${source_dir}/.bash_profile" ]; then
-    src="${source_dir}/.bash_profile"
-    dest="${dest_dir}/.profile"
-    if [ -L ${dest} ]; then
-        ln -snf -S ".bak" ${src} ${dest}
-        echo Existing link ${dest} renamed to ${dest}.bak
-    elif [ -f ${dest} ]; then
-        mv ${dest} ${dest}.bak
-        ln -s ${src} ${dest}
-        echo Existing regular file ${dest} renamed to ${dest}.bak
-    else
-        ln -s ${src} ${dest}
-    fi
-    echo File symbolic link created: ${dest}
+if $flag_force; then
+    echo forced
+else
+    make_link_safely
 fi
