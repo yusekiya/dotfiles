9fa1584b338028284d8b1a7b531d900fa231e23f
yusekiya <y.seki.14870@gmail.com>
2016-01-17 18:15:03 +0900

    Add settings for auctex on mac

--- .emacs.d/config/packages/my-auctex-config.el
+++ .emacs.d/config/packages/my-auctex-config.el
@@ -98,6 +98,99 @@
                                        TeX-run-discard-or-function t t :help "Run Adobe Acrobat Reader DC")))))
   )
 
+(when (equal system-type 'darwin)
+  (with-eval-after-load "latex"
+    (setq TeX-engine-alist '((pdfuptex "pdfupTeX"
+                                       "/Library/TeX/texbin/ptex2pdf -u -e -ot \"-kanji=utf8 -no-guess-input-enc %S %(mode)\""
+                                       "/Library/TeX/texbin/ptex2pdf -u -l -ot \"-kanji=utf8 -no-guess-input-enc %S %(mode)\""
+                                       "/Library/TeX/texbin/euptex")))
+    (setq japanese-TeX-engine-default 'pdfuptex)
+    (setq TeX-view-program-selection '((output-dvi "displayline")
+                                       (output-pdf "displayline")))
+    (setq japanese-LaTeX-default-style "jsarticle")
+    (dolist (command '("pTeX" "pLaTeX" "pBibTeX" "jTeX" "jLaTeX" "jBibTeX" "Mendex"))
+      (delq (assoc command TeX-command-list) TeX-command-list))
+    )
+  (setq preview-image-type 'dvipng)
+  (defun my:preview-scale-from-face ()
+    `(lambda nil
+       (/ ,(* (/ (preview-inherited-face-attribute
+                  'preview-reference-face
+                  :height 'default) 10.0) 1.1)
+          (preview-document-pt))))
+  (setq-default preview-scale-function #'my:preview-scale-from-face)
+  (custom-set-variables
+   '(preview-default-option-list
+     (quote
+      ("displaymath" "floats" "graphics" "textmath" "sections" "footnotes" "showlabels"))))
+  (setq TeX-source-correlate-method 'synctex)
+  (setq TeX-source-correlate-start-server t)
+  (add-hook 'LaTeX-mode-hook 'TeX-source-correlate-mode)
+  (add-hook 'LaTeX-mode-hook 'TeX-PDF-mode)
+  (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+  (add-hook 'LaTeX-mode-hook
+            (function (lambda ()
+                        (add-to-list 'TeX-command-list
+                                     '("Latexmk"
+                                       "/Library/TeX/texbin/latexmk %t"
+                                       TeX-run-TeX nil (latex-mode) :help "Run Latexmk"))
+                        (add-to-list 'TeX-command-list
+                                     '("Latexmk-upLaTeX-pdfdvi"
+                                       "/Library/TeX/texbin/latexmk -e '$latex=q/uplatex %%O %S %(mode) %%S/' -e '$bibtex=q/upbibtex %%O %%B/' -e '$biber=q/biber %%O --bblencoding=utf8 -u -U --output_safechars %%B/' -e '$makeindex=q/upmendex %%O -o %%D %%S/' -e '$dvipdf=q/dvipdfmx %%O -o %%D %%S/' -norc -gg -pdfdvi %t"
+                                       TeX-run-TeX nil (latex-mode) :help "Run Latexmk-upLaTeX-pdfdvi"))
+                        (add-to-list 'TeX-command-list
+                                     '("Latexmk-upLaTeX-pdfps"
+                                       "/Library/TeX/texbin/latexmk -e '$latex=q/uplatex %%O %S %(mode) %%S/' -e '$bibtex=q/upbibtex %%O %%B/' -e '$biber=q/biber %%O --bblencoding=utf8 -u -U --output_safechars %%B/' -e '$makeindex=q/upmendex %%O -o %%D %%S/' -e '$dvips=q/dvips %%O -z -f %%S | convbkmk -u > %%D/' -e '$ps2pdf=q/ps2pdf %%O %%S %%D/' -norc -gg -pdfps %t"
+                                       TeX-run-TeX nil (latex-mode) :help "Run Latexmk-upLaTeX-pdfps"))
+                        (add-to-list 'TeX-command-list
+                                     '("Latexmk-pdfLaTeX"
+                                       "/Library/TeX/texbin/latexmk -e '$pdflatex=q/pdflatex %%O %S %(mode) %%S/' -e '$bibtex=q/bibtex %%O %%B/' -e '$biber=q/biber %%O --bblencoding=utf8 -u -U --output_safechars %%B/' -e '$makeindex=q/makeindex %%O -o %%D %%S/' -norc -gg -pdf %t"
+                                       TeX-run-TeX nil (latex-mode) :help "Run Latexmk-pdfLaTeX"))
+                        (add-to-list 'TeX-command-list
+                                     '("Latexmk-LuaLaTeX"
+                                       "/Library/TeX/texbin/latexmk -e '$pdflatex=q/lualatex %%O %S %(mode) %%S/' -e '$bibtex=q/upbibtex %%O %%B/' -e '$biber=q/biber %%O --bblencoding=utf8 -u -U --output_safechars %%B/' -e '$makeindex=q/upmendex %%O -o %%D %%S/' -norc -gg -pdf %t"
+                                       TeX-run-TeX nil (latex-mode) :help "Run Latexmk-LuaLaTeX"))
+                        (add-to-list 'TeX-command-list
+                                     '("Latexmk-LuaJITLaTeX"
+                                       "/Library/TeX/texbin/latexmk -e '$pdflatex=q/luajitlatex %%O %S %(mode) %%S/' -e '$bibtex=q/upbibtex %%O %%B/' -e '$biber=q/biber %%O --bblencoding=utf8 -u -U --output_safechars %%B/' -e '$makeindex=q/upmendex %%O -o %%D %%S/' -norc -gg -pdf %t"
+                                       TeX-run-TeX nil (latex-mode) :help "Run Latexmk-LuaJITLaTeX"))
+                        (add-to-list 'TeX-command-list
+                                     '("Latexmk-XeLaTeX"
+                                       "/Library/TeX/texbin/latexmk -e '$pdflatex=q/xelatex %%O %S %(mode) %%S/' -e '$bibtex=q/upbibtex %%O %%B/' -e '$biber=q/biber %%O --bblencoding=utf8 -u -U --output_safechars %%B/' -e '$makeindex=q/upmendex %%O -o %%D %%S/' -norc -gg -pdf %t"
+                                       TeX-run-TeX nil (latex-mode) :help "Run Latexmk-XeLaTeX"))
+                        (add-to-list 'TeX-command-list
+                                     '("displayline"
+                                       "/Applications/Skim.app/Contents/SharedSupport/displayline %n %s.pdf \"%b\""
+                                       TeX-run-discard-or-function t t :help "Forward search with Skim"))
+                        (add-to-list 'TeX-command-list
+                                     '("Skim"
+                                       "/usr/bin/open -a Skim.app %s.pdf"
+                                       TeX-run-discard-or-function t t :help "Run Skim"))
+                        (add-to-list 'TeX-command-list
+                                     '("Preview"
+                                       "/usr/bin/open -a Preview.app %s.pdf"
+                                       TeX-run-discard-or-function t t :help "Run Preview"))
+                        (add-to-list 'TeX-command-list
+                                     '("TeXShop"
+                                       "/usr/bin/open -a TeXShop.app %s.pdf"
+                                       TeX-run-discard-or-function t t :help "Run TeXShop"))
+                        (add-to-list 'TeX-command-list
+                                     '("TeXworks"
+                                       "/Library/TeX/texbin/synctex view -i \"%n:0:%b\" -o %s.pdf -x \"/Applications/TeXworks.app/Contents/MacOS/TeXworks --position=%%{page+1} %%{output}\""
+                                       TeX-run-discard-or-function t t :help "Run TeXworks"))
+                        (add-to-list 'TeX-command-list
+                                     '("TeXstudio"
+                                       "/Library/TeX/texbin/synctex view -i \"%n:0:%b\" -o %s.pdf -x \"/Applications/texstudio.app/Contents/MacOS/texstudio --pdf-viewer-only --page %%{page+1} %%{output}\""
+                                       TeX-run-discard-or-function t t :help "Run TeXstudio"))
+                        (add-to-list 'TeX-command-list
+                                     '("Firefox"
+                                       "/usr/bin/open -a Firefox.app %s.pdf"
+                                       TeX-run-discard-or-function t t :help "Run Mozilla Firefox"))
+                        (add-to-list 'TeX-command-list
+                                     '("acroread"
+                                       "/usr/bin/open -a \"Adobe Acrobat Reader DC.app\" %s.pdf"
+                                       TeX-run-discard-or-function t t :help "Run Adobe Acrobat Reader DC")))))
+  )
 
 ;; Settings independent of OS
 (with-eval-after-load "latex"
