9f62be869b902caaa5b8a0d71cff7a797b96cb03
yusekiya <y.seki.14870@gmail.com>
2016-07-05 01:10:28 +0900

    Add key bind for evilnc in evil visual state

--- .emacs.d/config/packages/evil-config.el
+++ .emacs.d/config/packages/evil-config.el
@@ -98,6 +98,7 @@
     (define-key evil-normal-state-map " cl" 'evilnc-quick-comment-or-uncomment-to-the-line)
     (define-key evil-normal-state-map " ll" 'evilnc-quick-comment-or-uncomment-to-the-line)
     (define-key evil-normal-state-map " cc" 'evilnc-copy-and-comment-lines)
+    (define-key evil-visual-state-map " cc" 'evilnc-copy-and-comment-lines)
     (define-key evil-normal-state-map " cp" 'evilnc-comment-or-uncomment-paragraphs)
     (define-key evil-normal-state-map " cr" 'comment-or-uncomment-region)
     (define-key evil-normal-state-map " cv" 'evilnc-toggle-invert-comment-line-by-line)
