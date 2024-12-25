85d69a864bb1e5f32ec86840d8bb266eed090730
yusekiya <y.seki.14870@gmail.com>
2024-12-26 00:53:20 +0900

    Add basic config for neovim

---
+++ config/nvim/init.lua
@@ -0,0 +1,17 @@
+-- encoding
+vim.o.encoding = 'utf-8'
+vim.scriptencoding = 'utf-8'
+
+-- visual
+vim.o.ambiwidth = 'double'
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.autoindent = true
+vim.o.smartindent = true
+
+vim.o.visualbell = false
+vim.o.number = true
+vim.o.showmatch = true
+vim.o.matchtime = 1
