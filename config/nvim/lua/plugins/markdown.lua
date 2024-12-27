3b594f0730349d4dae81b24b9fb636e19d256624
yusekiya <y.seki.14870@gmail.com>
2024-12-27 15:45:09 +0900

    Modify build config for markdown preview in neovim

--- config/nvim/lua/plugins/markdown.lua
+++ config/nvim/lua/plugins/markdown.lua
@@ -3,6 +3,6 @@ return {
         "iamcco/markdown-preview.nvim",
         cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
         ft = { "markdown" },
-        build = function() vim.fn["mkdp#util#install"]() end,
+        build = ":call mkdp#util#install()",
     }
 }
