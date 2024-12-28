b1b521d80d69dd268e49c75ae56e2230f8ca261f
yusekiya <y.seki.14870@gmail.com>
2024-12-28 22:10:22 +0900

    Modify colorscheme for neovim

--- config/nvim/lua/plugins/appearance.lua
+++ config/nvim/lua/plugins/appearance.lua
@@ -1,23 +1,30 @@
 return {
     -- the colorscheme should be available when starting Neovim
     {
-        "shaunsingh/nord.nvim",
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
+        "gbprod/nord.nvim",
+        lazy = false,
+        priority = 1000,
         config = function()
-            -- load the colorscheme here
-            vim.cmd([[colorscheme nord]])
-            -- config
-            vim.g.nord_contrast = true
-            vim.g.nord_borders = false
-            vim.g.nord_disable_background = true
-            vim.g.nord_cursorline_transparent = true
-            vim.g.nord_enable_sidebar_background = false
-            vim.g.nord_italic = false
-            vim.g.nord_uniform_diff_background = true
-            vim.g.nord_bold = true
-            -- apply config
-            require('nord').set()
+            require("nord").setup({
+                diff = { mode = "fg" },
+                search = { theme = "vscode" },
+                borders = true,
+                transparent = true,
+                errors = { mode = "none" },
+                styles = {
+                    comments = { fg = "#7585a6" },
+                    keywords = {},
+                    functions = {},
+                    variables = {},
+                    errors = {},
+                    bufferline = {
+                        current = { bold = false },
+                        modified = { bold = false, italic = true },
+                    },
+                },
+                on_highlights = function(highlights, colors) end,
+            })
+            vim.cmd.colorscheme("nord")
         end,
     },
     -- status bar
