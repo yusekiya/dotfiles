205d6e02aaedc48a9b38ba18be127a881b1c9f93
yusekiya <y.seki.14870@gmail.com>
2024-12-29 00:28:07 +0900

    Add telescope plugin in neovim

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -1,12 +1,24 @@
 return {
     {
         "folke/noice.nvim",
+        lazy = true,
         event = "VeryLazy",
         opts = {
             -- add any options here
         },
         dependencies = {
             "MunifTanjim/nui.nvim",
-        }
-    }
+        },
+    },
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        lazy= true,
+        keys = {
+            {"<leader>ff", "<Cmd>Telescope find_files<cr>", mode = "n"},
+            {"<leader>fg", "<Cmd>Telescope live_grep<cr>", mode = "n"},
+            {"<leader>fb", "<Cmd>Telescope buffers<cr>", mode = "n"},
+            {"<leader>fh", "<Cmd>Telescope help_tags<cr>", mode = "n"},
+        },
+        dependencies = { 'nvim-lua/plenary.nvim' },
+    },
 }
