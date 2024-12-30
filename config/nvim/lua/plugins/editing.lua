ff7955abc335348ab420f7c7548f72a5061a0f68
yusekiya <y.seki.14870@gmail.com>
2024-12-31 02:04:02 +0900

    Add autopairs plugin to neovim

--- config/nvim/lua/plugins/editing.lua
+++ config/nvim/lua/plugins/editing.lua
@@ -46,4 +46,31 @@ return {
             require("leap").add_default_mappings(true)
         end
     },
+    -- auto pairs
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        dependencies = {
+            "hrsh7th/nvim-cmp",
+        },
+        config = function()
+            -- import nvim-autopairs
+            local autopairs = require("nvim-autopairs")
+            -- configure autopairs
+            autopairs.setup({
+                check_ts = true, -- enable treesitter
+                ts_config = {
+                    lua = { "string" }, -- don't add pairs in lua string treesitter nodes
+                    javascript = { "template_string" }, -- don't add pairs in javascript template_string treesitter nodes
+                    java = false, -- don't check treesitter on java
+                },
+            })
+            -- import nvim-autopairs completion functionality
+            local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+            -- import nvim-cmp plugin
+            local cmp = require("cmp")
+            -- make autopairs and completion work together
+            cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+        end,
+    },
 }
