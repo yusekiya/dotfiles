97fec4bc407e1d8a84c9f7ab4fc13116086e15b7
yusekiya <y.seki.14870@gmail.com>
2025-01-05 18:57:49 +0900

    Configure formatter for neovim

--- config/nvim/lua/plugins/editing.lua
+++ config/nvim/lua/plugins/editing.lua
@@ -1,90 +1,125 @@
 return {
-    -- toggle comment/uncomment
-    {
-        "numToStr/Comment.nvim",
-        opts = {
-            toggler = {
-                line = "gcc",
-                block = "gbc",
-            },
-            opleader = {
-                line = "gc",
-                block = "gb",
-            },
-        }
-    },
-    -- expand selection area
-    {
-        "terryma/vim-expand-region",
-        keys = {
-            { "v", "<Plug>(expand_region_expand)", mode = "v", desc = "Expand region recursively"},
-            { "<C-v>", "<Plug>(expand_region_shrink)", mode = "v", desc = "Shrink region recursively"},
-        },
-        lazy = true,
-    },
-    -- select/change surround parentheses
-    {
-        "kylechui/nvim-surround",
-        version = "*", -- Use for stability; omit to use `main` branch for the latest features
-        lazy = true,
-        event = "VeryLazy",
-        config = function()
-            require("nvim-surround").setup({
-                -- Configuration here, or leave empty to use defaults
-            })
-        end
-    },
-    -- sneak move
-    {
-        "ggandor/leap.nvim",
-        keys = {
-            { "f", "<Plug>(leap-forward)", mode = { "n", "x", "o" }, desc = "Leap Forward to" },
-            { "F", "<Plug>(leap-backward)", mode = { "n", "x", "o" }, desc = "Leap Backward to" },
-            { "gf", "<Plug>(leap-from-window)", mode = { "n", "x", "o" }, desc = "Leap from Windows" },
-        },
-    },
-    -- auto pairs
-    {
-        "windwp/nvim-autopairs",
-        event = "InsertEnter",
-        dependencies = {
-            "hrsh7th/nvim-cmp",
-        },
-        config = function()
-            -- import nvim-autopairs
-            local autopairs = require("nvim-autopairs")
-            -- configure autopairs
-            autopairs.setup({
-                check_ts = true, -- enable treesitter
-                ts_config = {
-                    lua = { "string" }, -- don't add pairs in lua string treesitter nodes
-                    javascript = { "template_string" }, -- don't add pairs in javascript template_string treesitter nodes
-                    java = false, -- don't check treesitter on java
-                },
-            })
-            -- import nvim-autopairs completion functionality
-            local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-            -- import nvim-cmp plugin
-            local cmp = require("cmp")
-            -- make autopairs and completion work together
-            cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-        end,
-    },
-    -- substitute a word in register
-    {
-        "gbprod/substitute.nvim",
-        lazy = true,
-        event = { "BufReadPre", "BufNewFile" },
-        config = function()
-            local substitute = require("substitute")
-            -- configure plugin
-            substitute.setup()
-            -- set keymaps
-            local keymap = vim.keymap
-            keymap.set("n", "s", substitute.operator, { desc = "Substitute with motion" })
-            keymap.set("n", "ss", substitute.line, { desc = "Substitute line" })
-            keymap.set("n", "S", substitute.eol, { desc = "Substitute to end of line" })
-            keymap.set("x", "s", substitute.visual, { desc = "Substitute in visual mode" })
-        end,
-    },
+	-- toggle comment/uncomment
+	{
+		"numToStr/Comment.nvim",
+		opts = {
+			toggler = {
+				line = "gcc",
+				block = "gbc",
+			},
+			opleader = {
+				line = "gc",
+				block = "gb",
+			},
+		},
+	},
+	-- expand selection area
+	{
+		"terryma/vim-expand-region",
+		keys = {
+			{ "v", "<Plug>(expand_region_expand)", mode = "v", desc = "Expand region recursively" },
+			{ "<C-v>", "<Plug>(expand_region_shrink)", mode = "v", desc = "Shrink region recursively" },
+		},
+		lazy = true,
+	},
+	-- select/change surround parentheses
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		lazy = true,
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
+	-- sneak move
+	{
+		"ggandor/leap.nvim",
+		keys = {
+			{ "f", "<Plug>(leap-forward)", mode = { "n", "x", "o" }, desc = "Leap Forward to" },
+			{ "F", "<Plug>(leap-backward)", mode = { "n", "x", "o" }, desc = "Leap Backward to" },
+			{ "gf", "<Plug>(leap-from-window)", mode = { "n", "x", "o" }, desc = "Leap from Windows" },
+		},
+	},
+	-- auto pairs
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/nvim-cmp",
+		},
+		config = function()
+			-- import nvim-autopairs
+			local autopairs = require("nvim-autopairs")
+			-- configure autopairs
+			autopairs.setup({
+				check_ts = true, -- enable treesitter
+				ts_config = {
+					lua = { "string" }, -- don't add pairs in lua string treesitter nodes
+					javascript = { "template_string" }, -- don't add pairs in javascript template_string treesitter nodes
+					java = false, -- don't check treesitter on java
+				},
+			})
+			-- import nvim-autopairs completion functionality
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			-- import nvim-cmp plugin
+			local cmp = require("cmp")
+			-- make autopairs and completion work together
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		end,
+	},
+	-- substitute a word in register
+	{
+		"gbprod/substitute.nvim",
+		lazy = true,
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local substitute = require("substitute")
+			-- configure plugin
+			substitute.setup()
+			-- set keymaps
+			local keymap = vim.keymap
+			keymap.set("n", "s", substitute.operator, { desc = "Substitute with motion" })
+			keymap.set("n", "ss", substitute.line, { desc = "Substitute line" })
+			keymap.set("n", "S", substitute.eol, { desc = "Substitute to end of line" })
+			keymap.set("x", "s", substitute.visual, { desc = "Substitute in visual mode" })
+		end,
+	},
+	-- formatting
+	{
+		"stevearc/conform.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local conform = require("conform")
+			conform.setup({
+				formatters_by_ft = {
+					javascript = { "prettier" },
+					typescript = { "prettier" },
+					javascriptreact = { "prettier" },
+					typescriptreact = { "prettier" },
+					css = { "prettier" },
+					html = { "prettier" },
+					json = { "prettier" },
+					yaml = { "prettier" },
+					markdown = { "prettier" },
+					lua = { "stylua" },
+					python = { "isort", "black" },
+				},
+				format_on_save = {
+					lsp_fallback = true,
+					async = false,
+					timeout_ms = 1000,
+				},
+			})
+			vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+				conform.format({
+					lsp_fallback = true,
+					async = false,
+					timeout_ms = 1000,
+				})
+			end, { desc = "Format file or range (in visual mode)" })
+		end,
+	},
 }
--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -3,10 +3,12 @@ return {
         "williamboman/mason.nvim",
         dependencies = {
             "williamboman/mason-lspconfig.nvim",
+            "WhoIsSethDaniel/mason-tool-installer.nvim",
         },
         config = function()
             local mason = require("mason")
             local mason_lspconfig = require("mason-lspconfig")
+            local mason_tool_installer = require("mason-tool-installer")
             mason.setup({
                 ui = {
                     icons = {
@@ -22,6 +24,15 @@ return {
                     "html",
                     "cssls",
                     "lua_ls",
+                },
+            })
+            mason_tool_installer.setup({
+                ensure_installed = {
+                    "prettier", -- prettier formatter
+                    "stylua", -- lua formatter
+                    "isort", -- python formatter
+                    "black", -- python formatter
+                    "pylint", -- python linter
                 },
             })
         end,
