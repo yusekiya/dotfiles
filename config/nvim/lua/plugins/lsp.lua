cadb557ff3923c10614469ae7f88ec2f142b4bd9
yusekiya <y.seki.14870@gmail.com>
2025-01-05 21:00:52 +0900

    Use ruff as python lsp in neovim

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -24,15 +24,14 @@ return {
           "html",
           "cssls",
           "lua_ls",
+          "ruff",
+          "pyright",
         },
       })
       mason_tool_installer.setup({
         ensure_installed = {
           "prettier", -- prettier formatter
           "stylua", -- lua formatter
-          "isort", -- python formatter
-          "black", -- python formatter
-          "pylint", -- python linter
         },
       })
     end,
