19ca41528d7b065d15fa6c32d437f8853ed48353
yusekiya <y.seki.14870@gmail.com>
2025-01-05 18:50:53 +0900

    Configure interface for diagnostics and others in neovim

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -145,5 +145,21 @@ return {
             -- or leave it empty to use the default settings
             -- refer to the configuration section below
         },
-    }
+    },
+    -- interface for diagnostics, references, and other lists
+    {
+        "folke/trouble.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
+        opts = {
+            focus = true,
+        },
+        cmd = "Trouble",
+        keys = {
+            { "<leader>xx", "<cmd>Trouble diagnostics toggle<CR>", desc = "Open trouble workspace diagnostics" },
+            { "<leader>xd", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", desc = "Open trouble document diagnostics" },
+            { "<leader>xq", "<cmd>Trouble quickfix toggle<CR>", desc = "Open trouble quickfix list" },
+            { "<leader>xl", "<cmd>Trouble loclist toggle<CR>", desc = "Open trouble location list" },
+            -- { "<leader>xt", "<cmd>Trouble todo toggle<CR>", desc = "Open todos in trouble" },
+        },
+    },
 }
