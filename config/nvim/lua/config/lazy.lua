98d908712c6c5b0b4434a2ecdaec270eb9443634
yusekiya <y.seki.14870@gmail.com>
2025-01-21 17:43:56 +0900

    Put together language-specific settings for neovim

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
   spec = {
     -- import your plugins
     { import = "plugins" },
+    { import = "plugins.languages" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
