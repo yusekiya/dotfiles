9505635fd11ead02649d6f067121b0e345fb6e18
yusekiya <y.seki.14870@gmail.com>
2025-01-21 23:54:59 +0900

    Add keymaps for git operation in neovim

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -222,8 +222,62 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPre" },
-    config = function()
-      require("gitsigns").setup()
-    end,
+    opts = {
+      on_attach = function(bufnr)
+        local gitsigns = require("gitsigns")
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map("n", "]c", function()
+          if vim.wo.diff then
+            vim.cmd.normal({ "]c", bang = true })
+          else
+            gitsigns.nav_hunk("next")
+          end
+        end, { desc = "go to next git hunk" })
+
+        map("n", "[c", function()
+          if vim.wo.diff then
+            vim.cmd.normal({ "[c", bang = true })
+          else
+            gitsigns.nav_hunk("prev")
+          end
+        end, { desc = "go to previous git hunk" })
+
+        -- Actions
+        map("n", "<leader>hs", gitsigns.stage_hunk, { desc = "stage git hunk" })
+        map("n", "<leader>hr", gitsigns.reset_hunk, { desc = "reset git hunk" })
+        map("v", "<leader>hs", function()
+          gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+        end, { desc = "stage selected hunk" })
+
+        map("v", "<leader>hr", function()
+          gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+        end, { desc = "reset selected hunk" })
+        map("n", "<leader>hS", gitsigns.stage_buffer, { desc = "stage whole buffer" })
+        map("n", "<leader>hR", gitsigns.reset_buffer, { desc = "reset whole buffer" })
+        map("n", "<leader>hp", gitsigns.preview_hunk, { desc = "preview git hunk" })
+        map("n", "<leader>hi", gitsigns.preview_hunk_inline, { desc = "preview git hunk inline" })
+
+        map("n", "<leader>hb", function()
+          gitsigns.blame_line({ full = true })
+        end, { desc = "git blame" })
+
+        map("n", "<leader>hd", gitsigns.diffthis, { desc = "git diff with index" })
+
+        map("n", "<leader>hD", function()
+          gitsigns.diffthis("~")
+        end, { desc = "git diff with previous commit" })
+
+        map("n", "<leader>hq", gitsigns.setqflist, { desc = "populate quickfix list with hunks" })
+        map("n", "<leader>hQ", function()
+          gitsigns.setqflist("all")
+        end, { desc = "populate quickfix list for all modified files" })
+      end,
+    },
   },
 }
