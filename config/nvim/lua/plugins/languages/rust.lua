dbc7b982e3a3aef2da58c025eb0260b85cccd5cd
yusekiya <y.seki.14870@gmail.com>
2025-01-25 18:57:33 +0900

    Add crates.nvim to neovim

--- config/nvim/lua/plugins/languages/rust.lua
+++ config/nvim/lua/plugins/languages/rust.lua
@@ -4,4 +4,12 @@ return {
     version = "^5", -- Recommended
     lazy = false, -- This plugin is already lazy
   },
+  {
+    "saecki/crates.nvim",
+    tag = "stable",
+    event = { "BufRead Cargo.toml" },
+    config = function()
+      require("crates").setup()
+    end,
+  },
 }
