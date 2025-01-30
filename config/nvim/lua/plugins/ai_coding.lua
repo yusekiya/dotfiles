cb81b08b3729711046846c4b2956846a3c225462
yusekiya <y.seki.14870@gmail.com>
2025-01-30 19:36:54 +0900

    Fix keymap for cody in neovim

--- config/nvim/lua/plugins/ai_coding.lua
+++ config/nvim/lua/plugins/ai_coding.lua
@@ -5,10 +5,10 @@ return {
     keys = {
       { "<leader>//", "<Cmd>CodyToggle<cr>", mode = "n", desc = "Toggle cody chat window" },
       { "<leader>/c", "<Cmd>CodyChat<cr>", mode = "n", desc = "Start a new chat" },
-      { "<leader>/a", "<Cmd>CodyAsk<cr>", mode = "x", desc = "Ask a question" },
-      { "<leader>/e", "<Cmd>CodyExplain<cr>", mode = "x", desc = "Explain current selection" },
-      { "<leader>/t", "<Cmd>CodyTask<cr>", mode = "x", desc = "Instruct cody to perform a task" },
-      { "<leader>/r", "<Cmd>CodyRestart<cr>", mode = "x", desc = "Restart cody" },
+      { "<leader>/r", "<Cmd>CodyRestart<cr>", mode = "n", desc = "Restart cody" },
+      { "<leader>/a", "<Cmd>'<,'>CodyAsk<cr>", mode = "x", desc = "Ask a question" },
+      { "<leader>/e", "<Cmd>'<,'>CodyExplain<cr>", mode = "x", desc = "Explain current selection" },
+      { "<leader>/t", "<Cmd>'<,'>CodyTask<cr>", mode = "x", desc = "Instruct cody to perform a task" },
     },
     dependencies = {
       "nvim-lua/plenary.nvim",
