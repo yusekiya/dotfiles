6352c99b5a0a84196f7df8440cb8c223459f12ed
yusekiya <y.seki.14870@gmail.com>
2025-03-31 00:52:45 +0900

    Use blink.cmp in neovim

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -120,65 +120,33 @@ return {
   },
   -- autocompletion
   {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-      "hrsh7th/cmp-buffer", -- source for text in buffer
-      "hrsh7th/cmp-path", -- source for file system paths
-      {
-        "L3MON4D3/LuaSnip",
-        version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-        build = "make install_jsregexp", -- install jsregexp (optional!)
+    "saghen/blink.cmp",
+    -- optional: provides snippets for the snippet source
+    dependencies = { "rafamadriz/friendly-snippets" },
+
+    -- use a release tag to download pre-built binaries
+    version = "1.*",
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      -- See :h blink-cmp-config-keymap for defining your own keymap
+      keymap = { preset = "enter" },
+
+      appearance = {
+        nerd_font_variant = "mono",
       },
-      "saadparwaiz1/cmp_luasnip", -- for autocompletion
-      "rafamadriz/friendly-snippets", -- useful snippets
-      "onsails/lspkind.nvim", -- vscode-like pictograms
+
+      -- (Default) Only show the documentation popup when manually triggered
+      completion = { documentation = { auto_show = false } },
+
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+
+      fuzzy = { implementation = "prefer_rust_with_warning" },
     },
-    config = function()
-      local cmp = require("cmp")
-      local luasnip = require("luasnip")
-      local lspkind = require("lspkind")
-      -- loads vscode style snippets
-      require("luasnip.loaders.from_vscode").lazy_load()
-      cmp.setup({
-        completion = {
-          completeopt = "menu,menuone,preview,noselect",
-        },
-        snippet = { -- configure how nvim-cmp interacts with snippet engine
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-p>"] = cmp.mapping.select_prev_item(), -- previous item suggestion
-          ["<Up>"] = cmp.mapping.select_prev_item(), -- previous item suggestion
-          ["<C-n>"] = cmp.mapping.select_next_item(), -- next item suggestion
-          ["<Down>"] = cmp.mapping.select_next_item(), -- next item suggestion
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = false }),
-        }),
-        -- sources for autocompletion
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" }, -- lsp
-          { name = "luasnip" }, -- snippets
-          { name = "cody" }, -- AI suggestions
-          { name = "buffer" }, -- text within current buffer
-          { name = "path" }, -- file system paths
-        }),
-        -- configure lspkind for vscode-like pictograms in completion nemu
-        formatting = {
-          format = lspkind.cmp_format({
-            maxwidth = 50,
-            ellipsis_char = "...",
-          }),
-          expandable_indicator = true,
-          fields = { "abbr", "kind", "menu" },
-        },
-      })
-    end,
+    opts_extend = { "sources.default" },
   },
   -- session manager
   {
