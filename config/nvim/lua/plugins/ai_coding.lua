1ba33ce74fd49503a511fd023bebb0de0ecc7340
yusekiya <y.seki.14870@gmail.com>
2025-04-01 23:52:59 +0900

    Modify keymap for nvim-aider

--- config/nvim/lua/plugins/ai_coding.lua
+++ config/nvim/lua/plugins/ai_coding.lua
@@ -3,16 +3,13 @@ return {
     "GeorgesAlkhouri/nvim-aider",
     cmd = { "AiderTerminalToggle", "AiderHealth" },
     keys = {
-      { "<leader>a/", "<cmd>AiderTerminalToggle<cr>", desc = "Open Aider" },
+      { "<leader>aa", "<cmd>AiderTerminalToggle<cr>", desc = "Open Aider" },
       { "<leader>as", "<cmd>AiderTerminalSend<cr>", desc = "Send to Aider", mode = { "n", "v" } },
       { "<leader>ac", "<cmd>AiderQuickSendCommand<cr>", desc = "Send Command To Aider" },
       { "<leader>ab", "<cmd>AiderQuickSendBuffer<cr>", desc = "Send Buffer To Aider" },
       { "<leader>a+", "<cmd>AiderQuickAddFile<cr>", desc = "Add File to Aider" },
       { "<leader>a-", "<cmd>AiderQuickDropFile<cr>", desc = "Drop File from Aider" },
       { "<leader>ar", "<cmd>AiderQuickReadOnlyFile<cr>", desc = "Add File as Read-Only" },
-      -- Example nvim-tree.lua integration if needed
-      { "<leader>a+", "<cmd>AiderTreeAddFile<cr>", desc = "Add File from Tree to Aider", ft = "NvimTree" },
-      { "<leader>a-", "<cmd>AiderTreeDropFile<cr>", desc = "Drop File from Tree from Aider", ft = "NvimTree" },
     },
     dependencies = {
       "folke/snacks.nvim",
