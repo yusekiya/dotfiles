d56e5a86c284af46b6bfb493c7297363932f8188
yusekiya <y.seki.14870@gmail.com>
2025-04-14 21:44:29 +0900

    Fix clipboard setting for neovim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -35,9 +35,11 @@ vim.g.maplocalleader = "\\"
 -- Sync clipboard between OS and Neovim.
 -- Function to set OSC 52 clipboard
 local function set_osc52_clipboard()
-  local function my_paste()
-    local content = vim.fn.getreg('"')
-    return vim.split(content, "\n")
+  local function paste()
+    return {
+      vim.fn.split(vim.fn.getreg(""), "\n"),
+      vim.fn.getregtype(""),
+    }
   end
 
   vim.g.clipboard = {
@@ -47,38 +49,23 @@ local function set_osc52_clipboard()
       ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
     },
     paste = {
-      ["+"] = my_paste,
-      ["*"] = my_paste,
+      ["+"] = paste,
+      ["*"] = paste,
     },
   }
 end
 
--- Check if the current session is a remote WezTerm session based on the WezTerm executable
-local function check_wezterm_remote_clipboard(callback)
-  local wezterm_executable = vim.uv.os_getenv("WEZTERM_EXECUTABLE")
-
-  if wezterm_executable and wezterm_executable:find("wezterm-mux-server", 1, true) then
-    callback(true) -- Remote WezTerm session found
-  else
-    callback(false) -- No remote WezTerm session
-  end
+function is_running_on_wezterm_mux_server()
+  return vim.uv.os_getenv("WEZTERM_EXECUTABLE"):find("wezterm-mux-server", 1, true) or nil
 end
 
--- Schedule the setting after `UiEnter` because it can increase startup-time.
-vim.schedule(function()
-  vim.opt.clipboard:append("unnamedplus")
-
-  -- Standard SSH session handling
-  if vim.uv.os_getenv("SSH_CLIENT") ~= nil or vim.uv.os_getenv("SSH_TTY") ~= nil then
-    set_osc52_clipboard()
-  else
-    check_wezterm_remote_clipboard(function(is_remote_wezterm)
-      if is_remote_wezterm then
-        set_osc52_clipboard()
-      end
-    end)
-  end
-end)
+if
+  vim.uv.os_getenv("SSH_CLIENT") ~= nil
+  or vim.uv.os_getenv("SSH_TTY") ~= nil
+  or is_running_on_wezterm_mux_server() ~= nil
+then
+  set_osc52_clipboard()
+end
 
 -- user defined commands
 vim.api.nvim_create_user_command("WClean", "%s/\\s\\+$//g", {})
