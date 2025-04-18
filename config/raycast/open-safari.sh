9a51b1b9e355aa725c205cfd1d708a63527bb334
yusekiya <y.seki.14870@gmail.com>
2025-04-18 16:36:01 +0900

    Add command to open new Safari window with Raycast

---
+++ config/raycast/open-safari.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open Safari
+# @raycast.mode silent
+# @raycast.packageName Navigation
+
+# Optional parameters:
+# @raycast.icon 🧭
+# @raycast.icon images/safari-icon.png
+
+# Documentation:
+# @raycast.description Opens new Safari window.
+
+open -n -a Safari
