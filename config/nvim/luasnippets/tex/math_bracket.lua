033df5694383dedf72642072aa9e5117910e8ba7
yusekiya <y.seki.14870@gmail.com>
2025-04-21 02:21:20 +0900

    Add latex snippets for brackets

---
+++ config/nvim/luasnippets/tex/math_bracket.lua
@@ -0,0 +1,165 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local i = ls.insert_node
+local f = ls.function_node
+local d = ls.dynamic_node
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+
+local ls_common = require("luasnip-utils.common")
+local get_visual = ls_common.get_visual
+local tex_utils = require("luasnip-utils.tex")
+
+return {
+  s(
+    {
+      trig = "([^%a])avg",
+      dscr = "average",
+      regTrig = true,
+      wordTrig = false,
+      condition = tex_utils.in_mathzone,
+      snippetType = "autosnippet",
+    },
+    fmta(
+      [[
+        <>\langle <> \rangle <>
+      ]],
+      {
+        f(function(_, snip)
+          return snip.captures[1]
+        end),
+        d(1, get_visual),
+        i(0),
+      }
+    )
+  ),
+  s(
+    {
+      trig = "([^%a])mod",
+      dscr = "mod",
+      regTrig = true,
+      wordTrig = false,
+      condition = tex_utils.in_mathzone,
+      snippetType = "autosnippet",
+    },
+    fmta(
+      [[
+        <>| <> | <>
+      ]],
+      {
+        f(function(_, snip)
+          return snip.captures[1]
+        end),
+        d(1, get_visual),
+        i(0),
+      }
+    )
+  ),
+  s(
+    {
+      trig = "([^%a])lrp",
+      dscr = "large parentheses",
+      regTrig = true,
+      wordTrig = false,
+      condition = tex_utils.in_mathzone,
+      snippetType = "autosnippet",
+    },
+    fmta(
+      [[
+        <>\left( <> \right)
+      ]],
+      {
+        f(function(_, snip)
+          return snip.captures[1]
+        end),
+        d(1, get_visual),
+      }
+    )
+  ),
+  s(
+    {
+      trig = "([^%a])lrc",
+      dscr = "large curly braces",
+      regTrig = true,
+      wordTrig = false,
+      condition = tex_utils.in_mathzone,
+      snippetType = "autosnippet",
+    },
+    fmta(
+      [[
+        <>\left\{ <> \right\}
+      ]],
+      {
+        f(function(_, snip)
+          return snip.captures[1]
+        end),
+        d(1, get_visual),
+      }
+    )
+  ),
+  s(
+    {
+      trig = "([^%a])lrb",
+      dscr = "large brackets",
+      regTrig = true,
+      wordTrig = false,
+      condition = tex_utils.in_mathzone,
+      snippetType = "autosnippet",
+    },
+    fmta(
+      [[
+        <>\left[ <> \right]
+      ]],
+      {
+        f(function(_, snip)
+          return snip.captures[1]
+        end),
+        d(1, get_visual),
+      }
+    )
+  ),
+  s(
+    {
+      trig = "([^%a])lr<",
+      dscr = "large angle brackets",
+      regTrig = true,
+      wordTrig = false,
+      condition = tex_utils.in_mathzone,
+      snippetType = "autosnippet",
+    },
+    fmt(
+      [[
+        {}\left< {} \right> {}
+      ]],
+      {
+        f(function(_, snip)
+          return snip.captures[1]
+        end),
+        d(1, get_visual),
+        i(0),
+      }
+    )
+  ),
+  s(
+    {
+      trig = "([^%a])lr|",
+      dscr = "large bars",
+      regTrig = true,
+      wordTrig = false,
+      condition = tex_utils.in_mathzone,
+      snippetType = "autosnippet",
+    },
+    fmta(
+      [[
+        <>\left| <> \right| <>
+      ]],
+      {
+        f(function(_, snip)
+          return snip.captures[1]
+        end),
+        d(1, get_visual),
+        i(0),
+      }
+    )
+  ),
+}
