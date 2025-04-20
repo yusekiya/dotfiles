4fdae78b4c6b75ab79b573be729adfd17dfeb6a6
yusekiya <y.seki.14870@gmail.com>
2025-04-21 02:24:10 +0900

    Add snippets for common environment

---
+++ config/nvim/luasnippets/tex/common_environment.lua
@@ -0,0 +1,30 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local i = ls.insert_node
+local fmta = require("luasnip.extras.fmt").fmta
+local rep = require("luasnip.extras").rep
+
+local line_begin = require("luasnip.extras.expand_conditions").line_begin
+
+return {
+  s(
+    {
+      trig = "beg",
+      dscr = "generic new environment",
+      snippetType = "autosnippet",
+      condition = line_begin,
+    },
+    fmta(
+      [[
+        \begin{<>}
+            <>
+        \end{<>}
+      ]],
+      {
+        i(1),
+        i(2),
+        rep(1),
+      }
+    )
+  ),
+}
