8dfba9e5a04d900a0aceb66a59afad67ce55ff97
yusekiya <y.seki.14870@gmail.com>
2025-04-26 22:32:00 +0900

    Use skim as pdf viewer on macos

--- config/nvim/lua/plugins/languages/tex.lua
+++ config/nvim/lua/plugins/languages/tex.lua
@@ -4,7 +4,9 @@ return {
     lazy = false,
     tag = "v2.16",
     init = function()
-      -- vim.g.vimtex_view_method = "zathura"
+      if vim.fn.has("mac") == 1 and vim.fn.executable("skimpdf") then
+        vim.g.vimtex_view_method = "skim"
+      end
       vim.g.vimtex_quickfix_enabled = 1
       vim.g.vimtex_quickfix_mode = 0
     end,
