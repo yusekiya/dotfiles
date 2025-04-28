fc2cdef5deeb40e38396404ff0710602c859e0d1
yusekiya <y.seki.14870@gmail.com>
2025-04-28 17:48:32 +0900

    Cleanup lsp config for neovim
    - Remove mason.nvim
    - Support new way to configure lsp in neovim 0.11

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,53 +1,9 @@
 return {
   {
-    "williamboman/mason.nvim",
-    dependencies = {
-      "williamboman/mason-lspconfig.nvim",
-      "WhoIsSethDaniel/mason-tool-installer.nvim",
-    },
-    config = function()
-      local mason = require("mason")
-      local mason_lspconfig = require("mason-lspconfig")
-      local mason_tool_installer = require("mason-tool-installer")
-      mason.setup({
-        ui = {
-          icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗",
-          },
-        },
-      })
-      mason_lspconfig.setup({
-        -- list of lsp servers for mason to install
-        ensure_installed = {
-          "html",
-          "cssls",
-          "lua_ls",
-          "ruff",
-          "pyright",
-        },
-      })
-      mason_tool_installer.setup({
-        ensure_installed = {
-          "prettier", -- prettier formatter
-          "stylua", -- lua formatter
-        },
-      })
-    end,
-  },
-  {
     "neovim/nvim-lspconfig",
     event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      { "antosha417/nvim-lsp-file-operations", config = true },
-      { "folke/neodev.nvim", opts = {} },
-    },
     config = function()
-      local lspconfig = require("lspconfig")
-      local mason_lspconfig = require("mason-lspconfig")
-      local cmp_nvim_lsp = require("cmp_nvim_lsp")
+      -- local lspconfig = require("lspconfig")
       local keymap = vim.keymap
 
       vim.diagnostic.config({ jump = { float = true } })
@@ -93,9 +49,6 @@ return {
           keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
         end,
       })
-
-      -- used to enable autocompletion
-      local capabilities = cmp_nvim_lsp.default_capabilities()
 
       -- Change the Diagnostic symbols in the sign column (gutter)
       local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
@@ -150,32 +103,21 @@ return {
       })
       vim.lsp.enable("texlab")
 
-      -- config for each language server
-      mason_lspconfig.setup_handlers({
-        -- default handler for installed servers
-        function(server_name)
-          lspconfig[server_name].setup({
-            capabilities = capabilities,
-          })
-        end,
-        -- lua
-        ["lua_ls"] = function()
-          lspconfig["lua_ls"].setup({
-            capabilities = capabilities,
-            settings = {
-              Lua = {
-                -- make the language server recognize "vim" global
-                diagnostics = {
-                  globals = { "vim" },
-                },
-                completion = {
-                  callSnippet = "Replace",
-                },
-              },
+      -- setting for lua
+      vim.lsp.config("lua_ls", {
+        settings = {
+          Lua = {
+            -- make the language server recognize "vim" global
+            diagnostics = {
+              globals = { "vim" },
             },
-          })
-        end,
+            completion = {
+              callSnippet = "Replace",
+            },
+          },
+        },
       })
+      vim.lsp.enable("lua_ls")
     end,
   },
 }
