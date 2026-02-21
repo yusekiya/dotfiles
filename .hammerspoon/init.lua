b448620957d6ce52eff11374f6ed3f1c278fe430
yusekiya <y.seki.14870@gmail.com>
2026-02-22 02:18:35 +0900

    Add window scroll keybind for paperwm.spoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -86,6 +86,7 @@ PaperWM:bindHotkeys({
 PaperWM.window_gap = 10
 PaperWM.swipe_fingers = 3
 PaperWM.drag_window = { "alt", "cmd" }
+PaperWM.scroll_window = { "ctrl", "cmd" }
 PaperWM.lift_window = { "ctrl", "cmd" }
 PaperWM.window_ratios = { 0.38195, 0.5, 0.61804 }
 
