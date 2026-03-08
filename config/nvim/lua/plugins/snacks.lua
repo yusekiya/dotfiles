cb3382529a7767e24f0489f5d3d4dc6e3e1a9ab3
yusekiya <y.seki.14870@gmail.com>
2026-03-08 14:21:48 +0900

    Config snacks.nvim via function
    For flexibility of configuration

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -2,46 +2,51 @@ return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
-  opts = {
-    bigfile = { enabled = false },
-    dashboard = { enabled = false },
-    explorer = { enabled = false },
-    indent = {
-      hl = "SnacksIndent",
-      scope = {
-        hl = "SnacksIndentScope",
+  config = function()
+    local opts = {
+      bigfile = { enabled = false },
+      dashboard = { enabled = false },
+      explorer = { enabled = false },
+      indent = {
+        hl = "SnacksIndent",
+        scope = {
+          hl = "SnacksIndentScope",
+        },
       },
-    },
-    input = { enabled = false },
-    picker = {
-      enabled = true,
-      win = {
-        input = {
-          keys = {
-            ["<c-a>"] = false,
-            ["<c-b>"] = false,
-            ["<c-f>"] = false,
-            ["<c-e>"] = false,
-            ["<c-d>"] = { "preview_scroll_down", mode = { "i", "n" } },
-            ["<c-u>"] = { "preview_scroll_up", mode = { "i", "n" } },
+      input = { enabled = false },
+      picker = {
+        enabled = true,
+        win = {
+          input = {
+            keys = {
+              ["<c-a>"] = false,
+              ["<c-b>"] = false,
+              ["<c-f>"] = false,
+              ["<c-e>"] = false,
+              ["<c-d>"] = { "preview_scroll_down", mode = { "i", "n" } },
+              ["<c-u>"] = { "preview_scroll_up", mode = { "i", "n" } },
+              ["<c-s>"] = { "edit_split", mode = { "i", "n" } },
+              ["<c-v>"] = { "edit_vsplit", mode = { "i", "n" } },
+            },
           },
         },
       },
-    },
-    notifier = { enabled = false },
-    quickfile = { enabled = false },
-    scope = { enabled = false },
-    scroll = { enabled = false },
-    statuscolumn = { enabled = false },
-    terminal = {},
-    win = {
-      position = "float",
-      height = 0.9,
-      width = 0.9,
-      zindex = 50,
-    },
-    words = { enabled = false },
-  },
+      notifier = { enabled = false },
+      quickfile = { enabled = false },
+      scope = { enabled = false },
+      scroll = { enabled = false },
+      statuscolumn = { enabled = false },
+      terminal = {},
+      win = {
+        position = "float",
+        height = 0.9,
+        width = 0.9,
+        zindex = 50,
+      },
+      words = { enabled = false },
+    }
+    require("snacks").setup(opts)
+  end,
   keys = {
     -- find
     {
