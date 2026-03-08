27d144b48cff5935eb5062c9d189bd9620aa4a54
yusekiya <y.seki.14870@gmail.com>
2026-03-09 00:28:16 +0900

    Update neovim config for tex

--- config/nvim/lua/plugins/languages/tex.lua
+++ config/nvim/lua/plugins/languages/tex.lua
@@ -4,18 +4,21 @@ return {
     lazy = false,
     tag = "v2.16",
     init = function()
-      if vim.fn.has("mac") == 1 and vim.fn.executable("skimpdf") then
+      if vim.fn.executable("zathura") and vim.fn.has("mac") == 0 then
+        -- Disable zathura by default on mac because it required additional config to work
+        vim.g.vimtex_view_method = "zathura"
+      elseif vim.fn.executable("skimpdf") then
         vim.g.vimtex_view_method = "skim"
       end
       vim.g.vimtex_quickfix_enabled = 1
       vim.g.vimtex_quickfix_mode = 0
     end,
   },
-  {
-    "let-def/texpresso.vim",
-    ft = "tex",
-    config = function()
-      require('texpresso').attach()
-    end
-  }
+  -- {
+  --   "let-def/texpresso.vim",
+  --   ft = "tex",
+  --   config = function()
+  --     require("texpresso").attach()
+  --   end,
+  -- },
 }
