52b4fa4225727720cc4409452ced93da88416d76
yusekiya <y.seki.14870@gmail.com>
2026-03-09 04:32:03 +0900

    Tweaking

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,7 +1,7 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
+    event = { "FileType", "BufNewFile" },
     dependencies = {
       "folke/snacks.nvim",
     },
