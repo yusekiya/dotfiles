00c552fc3d16bd5ede31c80a42dc2e7c9e52e32f
yusekiya <y.seki.14870@gmail.com>
2026-03-20 14:06:36 +0900

    Load molten-nvim when running Molten commands

--- config/nvim/lua/plugins/languages/jupyter.lua
+++ config/nvim/lua/plugins/languages/jupyter.lua
@@ -3,7 +3,7 @@ return {
     "benlubas/molten-nvim",
     version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
     dependencies = { "3rd/image.nvim" },
-    event = "CmdLineEnter",
+    cmd = { "MoltenInit", "MoltenInfo" },
     build = ":UpdateRemotePlugins",
     init = function()
       -- options
