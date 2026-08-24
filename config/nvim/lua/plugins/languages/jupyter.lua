dae2a4c51af3feb273e68aeec7d13da3b17559f0
yusekiya <y.seki.14870@gmail.com>
2026-08-24 14:49:28 +0900

    Add ipynb.nvim plugin

--- config/nvim/lua/plugins/languages/jupyter.lua
+++ config/nvim/lua/plugins/languages/jupyter.lua
@@ -1,7 +1,17 @@
 return {
   {
     "GCBallesteros/jupytext.nvim",
+    event = { "BufRead *.ipynb", "BufNewFile *.ipynb" },
     config = true,
     opts = { style = "percent" },
+  },
+  {
+    "ajbucci/ipynb.nvim",
+    event = { "BufRead *.ipynb", "BufNewFile *.ipynb" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "neovim/nvim-lspconfig",
+    },
+    opts = {},
   },
 }
